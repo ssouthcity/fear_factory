85e3659e632d333b0774153ba927d654c92e3bd9
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2025-07-24 23:22:25 +0200

    feat: add hotbar and entity spawning

---
+++ src/hotbar.rs
@@ -0,0 +1,93 @@
+use bevy::prelude::*;
+
+use crate::BuildingType;
+
+pub fn plugin(app: &mut App) {
+    app.register_type::<HotbarSelection>();
+    app.register_type::<HotbarSlot>();
+
+    app.init_resource::<HotbarSelection>();
+
+    app.add_systems(Startup, spawn_hotbar);
+
+    app.add_systems(
+        Update,
+        (handle_hotbar_input, highlight_selected_slot).chain(),
+    );
+}
+
+#[derive(Resource, Default, Reflect)]
+#[reflect(Resource)]
+pub struct HotbarSelection(pub BuildingType);
+
+#[derive(Component, Default, Reflect)]
+#[reflect(Component)]
+struct HotbarSlot(BuildingType);
+
+fn spawn_hotbar(mut commands: Commands) {
+    commands.spawn((
+        Name::new("Taskbar Container"),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::ZERO,
+            width: Val::Percent(100.0),
+            height: Val::Px(128.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            column_gap: Val::Px(8.0),
+            row_gap: Val::Px(8.0),
+            ..default()
+        },
+        children![
+            hotbar_slot(BuildingType::Miner, Color::linear_rgb(0.5, 0.0, 0.0)),
+            hotbar_slot(BuildingType::PowerPole, Color::linear_rgb(0.0, 0.0, 0.5)),
+        ],
+    ));
+}
+
+fn hotbar_slot(slot: BuildingType, color: Color) -> impl Bundle {
+    (
+        Name::new(format!("Hotbar Slot")),
+        HotbarSlot(slot),
+        Node {
+            width: Val::Px(64.0),
+            height: Val::Px(64.0),
+            ..default()
+        },
+        BackgroundColor(color),
+        BorderColor(Color::linear_rgb(0.8, 0.8, 0.0)),
+    )
+}
+
+fn handle_hotbar_input(keys: Res<ButtonInput<KeyCode>>, mut selected: ResMut<HotbarSelection>) {
+    let keycodes = [
+        (KeyCode::Digit1, BuildingType::Miner),
+        (KeyCode::Digit2, BuildingType::PowerPole),
+        // KeyCode::Digit3,
+        // KeyCode::Digit4,
+        // KeyCode::Digit5,
+        // KeyCode::Digit6,
+        // KeyCode::Digit7,
+        // KeyCode::Digit8,
+        // KeyCode::Digit9,
+    ];
+
+    for (key, building) in keycodes {
+        if keys.just_pressed(key) {
+            selected.0 = building;
+        }
+    }
+}
+
+fn highlight_selected_slot(
+    hotbar_slots: Query<(&HotbarSlot, &mut Node)>,
+    selection: Res<HotbarSelection>,
+) {
+    for (slot, mut node) in hotbar_slots {
+        if slot.0 == selection.0 {
+            node.border = UiRect::all(Val::Px(4.0));
+        } else {
+            node.border = UiRect::DEFAULT;
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,81 @@
 use bevy::prelude::*;
 
+use crate::hotbar::HotbarSelection;
+
+mod hotbar;
+
 pub struct FactoryGamePlugin;
 
 impl Plugin for FactoryGamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultPlugins);
 
-        app.insert_resource(ClearColor(Color::linear_rgb(0.25, 0.4, 0.0)));
+        app.add_plugins(hotbar::plugin);
 
-        app.add_systems(Startup, setup);
+        app.insert_resource(ClearColor(Color::linear_rgb(0.1, 0.1, 0.1)));
+
+        app.add_systems(Startup, (setup_camera, setup_world));
+
+        app.add_observer(drag_camera);
     }
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
 
+fn setup_world(mut commands: Commands) {
     commands
         .spawn((
-            Sprite::from_color(Color::linear_rgb(0.0, 0.0, 1.0), Vec2::splat(1028.0)),
+            Sprite::from_color(Color::linear_rgb(0.25, 0.25, 0.0), Vec2::splat(1028.0)),
             Pickable::default(),
         ))
         .observe(spawn_building);
 }
 
-fn spawn_building(trigger: Trigger<Pointer<Click>>, mut commands: Commands) {
-    commands.spawn((
-        Transform::from_translation(trigger.event().hit.position.unwrap_or_default()),
-        Sprite::from_color(Color::linear_rgb(1.0, 0.0, 0.0), Vec2::splat(64.0)),
-    ));
+fn drag_camera(
+    trigger: Trigger<Pointer<Drag>>,
+    mut camera_position: Single<&mut Transform, With<Camera>>,
+) {
+    let event = trigger.event();
+
+    if event.button != PointerButton::Secondary {
+        return;
+    }
+
+    camera_position.translation += event.delta.extend(0.0) * Vec3::new(-1.0, 1.0, 1.0) * 0.2;
 }
+
+fn spawn_building(
+    trigger: Trigger<Pointer<Click>>,
+    mut commands: Commands,
+    selected_buildable: Res<HotbarSelection>,
+) {
+    if trigger.event().button != PointerButton::Primary {
+        return;
+    }
+
+    let mouse_position = trigger.event().hit.position.unwrap_or_default();
+
+    let mut entity = commands.spawn(Transform::from_translation(mouse_position));
+
+    match selected_buildable.0 {
+        BuildingType::Miner => entity.insert(Miner),
+        BuildingType::PowerPole => entity.insert(PowerPole),
+    };
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Reflect)]
+pub enum BuildingType {
+    #[default]
+    Miner,
+    PowerPole,
+}
+
+#[derive(Component)]
+#[require(Sprite::from_color(Color::linear_rgb(0.5, 0.0, 0.0), Vec2::splat(64.0)))]
+struct Miner;
+
+#[derive(Component)]
+#[require(Sprite::from_color(Color::linear_rgb(0.0, 0.0, 0.5), Vec2::splat(64.0)))]
+struct PowerPole;
