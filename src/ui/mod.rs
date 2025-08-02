af1c93fbcd31e453b9ce0fa5615f6c4bd6a6ad5b
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2025-08-02 05:14:52 +0200

    feat: add y-sort

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,6 +6,16 @@ mod hotbar;
 pub use highlight::Highlightable;
 pub use hotbar::HotbarSelection;
 
+use crate::FactorySystems;
+
 pub fn plugin(app: &mut App) {
     app.add_plugins((highlight::plugin, hotbar::plugin));
+
+    app.add_systems(Update, y_sort.in_set(FactorySystems::UI));
+}
+
+fn y_sort(query: Query<&mut Transform, With<Sprite>>) {
+    for mut transform in query {
+        transform.translation.z = 1.0 - transform.translation.y.atan() / std::f32::consts::PI + 0.5;
+    }
 }
