984cd16cce0822926b817adc5beb01c15bdef7d8
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2025-10-06 16:37:18 +0200

    feat: add harvesting according to design spec

--- src/gameplay/world/deposit.rs
+++ src/gameplay/world/deposit.rs
@@ -1,15 +1,20 @@
-use bevy::{asset::LoadedFolder, prelude::*};
+use std::time::Duration;
+
+use bevy::{asset::LoadedFolder, prelude::*, time::common_conditions::on_timer};
 use bevy_ecs_tilemap::tiles::TilePos;
 use rand::Rng;
 use serde::Deserialize;
 
 use crate::{
-    assets::{loaders::toml::TomlAssetPlugin, tracking::LoadResource},
+    assets::{indexing::IndexMap, loaders::toml::TomlAssetPlugin, tracking::LoadResource},
     gameplay::{
+        FactorySystems,
+        item::{Item, Quantity, assets::ItemDef},
+        recipe::{OutputOf, Outputs},
         sprite_sort::{YSortSprite, ZIndexSprite},
         world::{
             WorldSpawnSystems,
-            construction::Constructions,
+            construction::{Constructions, StructureConstructed},
             tilemap::{CHUNK_SIZE, TILE_SIZE, coord::Coord},
         },
     },
@@ -25,13 +30,23 @@ pub fn plugin(app: &mut App) {
         OnEnter(Screen::Gameplay),
         spawn_deposits.in_set(WorldSpawnSystems::SpawnDeposits),
     );
+
+    app.add_systems(
+        Update,
+        (
+            attach_harvested_nodes.in_set(FactorySystems::PostConstruction),
+            harvest_node
+                .in_set(FactorySystems::Harvest)
+                .run_if(on_timer(Duration::from_millis(10))),
+        ),
+    );
 }
 
 #[derive(Asset, Deserialize, Reflect)]
 pub struct DepositDef {
     pub id: String,
     pub name: String,
-    pub recipe_id: String,
+    pub item_id: String,
     pub quantity: u32,
 }
 
@@ -53,9 +68,16 @@ impl FromWorld for DepositAssets {
     }
 }
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
-pub struct DepositRecipe(pub String);
+pub struct Deposit {
+    manifest: Handle<DepositDef>,
+    quantity: u32,
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+pub struct HarvestingFrom(pub Vec<Entity>);
 
 fn spawn_deposits(
     mut commands: Commands,
@@ -65,7 +87,7 @@ fn spawn_deposits(
 ) {
     let mut rng = rand::rng();
 
-    for (_, deposit) in deposit_definitions.iter() {
+    for (asset_id, deposit) in deposit_definitions.iter() {
         for _ in 0..deposit.quantity {
             let tile_pos = TilePos::new(
                 rng.random_range(0..CHUNK_SIZE.x),
@@ -83,11 +105,99 @@ fn spawn_deposits(
                         custom_size: Vec2::new(TILE_SIZE.x, TILE_SIZE.y).into(),
                         ..default()
                     },
-                    DepositRecipe(deposit.recipe_id.clone()),
+                    Deposit {
+                        manifest: asset_server.get_id_handle(asset_id).unwrap(),
+                        quantity: rng.random_range(50..200),
+                    },
                 ))
                 .id();
 
             constructions.insert(tile_pos.into(), entity);
+        }
+    }
+}
+
+fn attach_harvested_nodes(
+    mut structures_constructed: MessageReader<StructureConstructed>,
+    structure_query: Query<(&Name, &Coord)>,
+    deposit_query: Query<Entity, With<Deposit>>,
+    constructions: Res<Constructions>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    item_index: Res<IndexMap<ItemDef>>,
+) {
+    for StructureConstructed(structure) in structures_constructed.read() {
+        let Ok((name, coord)) = structure_query.get(*structure) else {
+            continue;
+        };
+
+        if "Harvester" != name.to_string() {
+            continue;
+        }
+
+        let neighboring_coords: Vec<UVec2> = vec![
+            coord.0.saturating_sub(UVec2::new(0, 1)),
+            coord.0.saturating_sub(UVec2::new(1, 0)),
+            coord.0.saturating_add(UVec2::new(0, 1)),
+            coord.0.saturating_add(UVec2::new(1, 0)),
+        ];
+
+        let deposits: Vec<Entity> = neighboring_coords
+            .into_iter()
+            .filter_map(|c| constructions.get(&c))
+            .filter(|e| deposit_query.contains(**e))
+            .map(|e| *e)
+            .collect();
+
+        commands.entity(*structure).insert(HarvestingFrom(deposits));
+
+        let harvestables = ["flora_a", "fauna_a"];
+
+        for harvestable in harvestables {
+            let item_id = item_index.get(harvestable).unwrap();
+            let handle = asset_server.get_id_handle(*item_id).unwrap();
+
+            commands.spawn((
+                Name::new("Output"),
+                Item(handle),
+                Quantity(0),
+                OutputOf(*structure),
+            ));
+        }
+    }
+}
+
+fn harvest_node(
+    harvester_query: Query<(&HarvestingFrom, &Outputs)>,
+    mut deposit_query: Query<(Entity, &Coord, &mut Deposit)>,
+    mut output_query: Query<&mut Quantity, With<OutputOf>>,
+    mut commands: Commands,
+    mut constructions: ResMut<Constructions>,
+) {
+    for (harvesting_from, outputs) in harvester_query {
+        let Some(node_to_harvest) = harvesting_from.0.first() else {
+            continue;
+        };
+
+        let Ok((deposit_entity, coord, mut deposit)) = deposit_query.get_mut(*node_to_harvest)
+        else {
+            continue;
+        };
+
+        let Some(output) = outputs.iter().next() else {
+            continue;
+        };
+
+        let Ok(mut quantity) = output_query.get_mut(output) else {
+            continue;
+        };
+
+        deposit.quantity = deposit.quantity.saturating_sub(1);
+        quantity.0 = quantity.0.saturating_add(1);
+
+        if deposit.quantity == 0 {
+            commands.entity(deposit_entity).despawn();
+            constructions.remove(coord);
         }
     }
 }
