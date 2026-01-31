d990e3bae799269dc951a10ab105095d0c90071f
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2026-01-31 22:18:29 +0100

    feat: add splash screen

--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -2,16 +2,18 @@ use bevy::prelude::*;
 
 mod gameplay;
 mod loading;
+mod splash;
 
 pub fn plugin(app: &mut App) {
     app.init_state::<Screen>();
 
-    app.add_plugins((loading::plugin, gameplay::plugin));
+    app.add_plugins((loading::plugin, gameplay::plugin, splash::plugin));
 }
 
 #[derive(States, Hash, Debug, PartialEq, Eq, Clone, Copy, Default)]
 pub enum Screen {
     #[default]
+    Splash,
     Loading,
     Gameplay,
 }
